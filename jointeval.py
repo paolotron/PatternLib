7060e5ca47f5f69a039f45b50d8a4cdc99e361e9
Paolo Rabino <s283282@studenti.polito.it>
2021-06-11 18:13:50 +0200

    added jointer saver

--- jointeval.py
+++ jointeval.py
@@ -10,6 +10,8 @@ from tiblib.validation import grid_search, kfold_split, err_rate
 
 def testjoint():
     train, train_labels, test, test_labels = get_pulsar_data(labels=True)
+    scores_f = []
+    labels_f = []
     pip = Pipeline([StandardScaler(),
                     Lda(n_dim=6),
                     Jointer([
@@ -17,9 +19,9 @@ def testjoint():
                         cl.GaussianMixture(alpha=0.1, N=2, tied=True),
                         cl.LogisticRegression(norm_coeff=0.1),
                         cl.SVM(c=1, k=0, ker='Poly', paramker=[2, 1])
-                        ]),
+                    ]),
                     StandardScaler()
-        ])
+                    ])
     for hyper in grid_search({'norm_coeff': [.1, .2, .5, 1]}):
         pip.add_step(cl.LogisticRegression(**hyper))
         err_r = 0
@@ -34,10 +36,14 @@ def testjoint():
         m1 = minDetectionCost(scores, labels, 1000, pi1=0.5)[0]
         m2 = minDetectionCost(scores, labels, 1000, pi1=0.1)[0]
         m3 = minDetectionCost(scores, labels, 1000, pi1=0.9)[0]
+        scores_f.append(scores)
+        labels_f.append(labels)
         print(pip)
-        print(err_r/5)
+        print(err_r / 5)
         pip.rem_step()
         print(f"{round(m1, 3)} & {round(m2, 3)} & {round(m3, 3)}")
+    np.save("result/jointResults/jointlabels", scores_f)
+    np.save("result/jointResults/jointscores", labels_f)
 
 
 if __name__ == "__main__":
